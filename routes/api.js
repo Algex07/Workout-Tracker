1163b45f61b79e1aaf64a9791d26d22195ab55aa
Amara <amar_algu@hotmail.com>
2021-11-12 03:50:52 -0500

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+
+const router = require("express").Router();
+// const Workout = require("../models/workout.js")
+
+router.post("/api/workouts", (req, res)=>{
+   // Workout.create({})
+})
+
+router.get('/api/workouts')
---
+++ server.js
@@ -0,0 +1,7 @@
+// create workout model 
+// get the view.js working by getting the exercise.html and stats.html (reference class work)
+// work on api.js to get api routes working 
+// referencing api.js in the PUBLIC folder match your routes in api.js in ROUTES folder to connect those end points
+// look up MONGODB aggregate functions
+
+// GET SERVER.JS running first
